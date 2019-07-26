ae8b5a214b0f218f341bfc2681bdec543e457412
Mike Luttikhuis <mikeluttikhuis@gmail.com>
2019-07-26 12:13:11 +0200

    Fixes + added UA

--- bier_api.py
+++ bier_api.py
@@ -19,34 +19,36 @@ app.logger.addHandler(handler)
 @app.route('/')
 @cache.cached(timeout=14400)
 def index():
-        r_shop = requests.get('https://www.biernet.nl/extra/app/V3_3.3.4/winkel.php')
-        r_shop = json.loads(r_shop.text)
-        r_discount = requests.get('https://www.biernet.nl/extra/app/V3_3.3.4/aanbieding.php')
-        r_discount = json.loads(r_discount.text)
-        r_soort = requests.get('https://www.biernet.nl/extra/app/V3_3.3.4/soort.php')
-        r_soort = json.loads(r_soort.text)
+	payload = {'d': ''}
+	headers = {'User-Agent': 'nl.Biernet.iOS.app/V3'}
+	r_shop = requests.get('https://www.biernet.nl/extra/app/V3_3.3.4/winkel.php', params=payload, headers=headers)
+	r_shop = json.loads(r_shop.text)
+	r_discount = requests.get('https://www.biernet.nl/extra/app/V3_3.3.4/aanbieding.php', params=payload, headers=headers)
+	r_discount = json.loads(r_discount.text)
+	r_soort = requests.get('https://www.biernet.nl/extra/app/V3_3.3.4/soort.php', params=payload, headers=headers)
+	r_soort = json.loads(r_soort.text)
 
-        shops = {
-                "1": "Albert Heijn",
-                "9": "Coop",
-                "14": "Gall en Gall",
-                "5": "Jumbo"
-        }
+	shops = {
+		"1": "Albert Heijn",
+		"9": "Coop",
+		"14": "Gall en Gall",
+		"5": "Jumbo"
+	}
 
-        brands = {
-                "1141": "Grolsch Premium Pilsener Krat 24x0,3L",
-                "13": "Grolsch Premium Pilsener Krat 16x0,45L",
-                "19": "Hertog Jan Pilsener Krat 24x0,3L",
-                "246": "Warsteiner Premium Pilsener Krat 24x0,33L",
-                "26": "Warsteiner Premium Pilsener Krat 24x0,3L"
-        }
+	brands = {
+		"1141": "Grolsch Premium Pilsener Krat 24x0,3L",
+		"13": "Grolsch Premium Pilsener Krat 16x0,45L",
+		"19": "Hertog Jan Pilsener Krat 24x0,3L",
+		"246": "Warsteiner Premium Pilsener Krat 24x0,33L",
+		"26": "Warsteiner Premium Pilsener Krat 24x0,3L"
+	}
 
-        discounts = []
-        for dc in r_discount:
-                if dc["soort_uid"] in brands and dc["winkel_uid"] in shops:
-                        discounts.append('Winkel: '+shops[dc["winkel_uid"]]+'</br>')
-                        discounts.append('Merk: '+dc["aantal"]+'x '+brands[dc["soort_uid"]]+'</br>')
-                        discounts.append('Prijs: €'+dc["voorprijs"]+'</br>')
-                        discounts.append('Geldig t/m: '+dc["einddatum"]+'</br>')
-                        discounts.append('</br>')
-        return ''.join(discounts)
+	discounts = []
+	for dc in r_discount:
+		if dc["soort_uid"] in brands and dc["winkel_uid"] in shops:
+			discounts.append('Winkel: '+shops[dc["winkel_uid"]]+'</br>')
+			discounts.append('Merk: '+dc["aantal"]+'x '+brands[dc["soort_uid"]]+'</br>')
+			discounts.append('Prijs: €'+dc["voorprijs"]+'</br>')
+			discounts.append('Geldig t/m: '+dc["einddatum"]+'</br>')
+			discounts.append('</br>')
+	return ''.join(discounts)
